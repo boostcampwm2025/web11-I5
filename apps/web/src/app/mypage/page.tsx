32e34882f73db801ec2e74c53d2b01f3585b8b8f
Choi JunHo <duekzim@gmail.com>
2026-01-20 21:44:03 +0900

    style: mypage의 page.tsx, layout.tsx 디자인 반영

--- apps/web/src/app/mypage/page.tsx
+++ apps/web/src/app/mypage/page.tsx
@@ -1,82 +1,73 @@
-import { BarChart3 } from "lucide-react";
-import GraphView from "./_components/graph-view/graph-view";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/tabs/tabs";
+import { BookText, Brush, CircleCheckBig } from "lucide-react";
 import UserStatsCard from "./_components/user-stats-card/user-stats-card";
-import VoronoiStreak from "./_components/voronoi-streak/voronoi-streak";
 import { mockGraphData } from "./_constants/graph-mock";
+import GraphContent from "./_contents/graph-content";
+import SolvedContent from "./_contents/solved-content";
+import StreakContent from "./_contents/streak-content";
 
 async function MyPage() {
   //실제 api 연동시 여기에 데이터 패칭 로직
   const userData = {
     id: 1,
     nickname: "김개발",
+    email: "@malman_dev",
     totalPoint: 200,
-    totalScore: 500,
+    role: "FRONTEND DEV",
   };
   const consecutiveDayCount = 0;
   const mockData = mockGraphData;
-  // const mockData = { nodes: [], edges: [] };
-  const streakCount = 0;
+  const streakCount = 40;
   const imageSrc = "/starry-night.jpg";
   return (
-    <main className="max-w-4xl mx-auto p-8 space-y-8 min-h-screen  ">
-      <section className="space-y-4">
-        <div className="space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            마이페이지
-          </h1>
-          <p className="text-muted-foreground">학습 통계를 확인하세요</p>
-        </div>
-
-        <UserStatsCard
-          nickname={userData.nickname}
-          consecutiveDayCount={consecutiveDayCount}
-          totalPoint={userData.totalPoint}
-          totalScore={userData.totalScore}
-        />
-      </section>
-
-      <section className="w-full flex flex-col gap-6">
-        <div className="bg-white rounded-lg border border-gray-200 shadow-sm p-6">
-          <h2 className="text-xl font-semibold text-gray-900 mb-4">
-            학습 그래프
-          </h2>
-          <div className="h-100">
-            {mockData.nodes.length === 0 && mockData.edges.length === 0 ? (
-              <div className="flex flex-col h-full w-full items-center justify-center py-12 text-center">
-                <div className="w-16 h-16 mb-4 rounded-full bg-gray-100 flex items-center justify-center">
-                  <BarChart3 className="w-8 h-8 text-gray-400" />
-                </div>
-                <h3 className="text-lg font-semibold text-gray-900 mb-2">
-                  아직 학습 데이터가 없습니다
-                </h3>
-                <p className="text-sm text-gray-500">
-                  문제를 풀면 학습 그래프가 생성됩니다
-                </p>
-              </div>
-            ) : (
-              <GraphView mockData={mockData} />
-            )}
-          </div>
-        </div>
-
-        <div className="bg-white rounded-lg border border-gray-200 shadow-sm p-6">
-          <div className="flex items-center justify-between mb-4">
-            <h2 className="text-xl font-semibold text-gray-900">학습 스트릭</h2>
-            <section className="flex gap-1">
-              <span className="text-sm font-medium text-blue-600 bg-blue-50 px-3 py-1 rounded-full">
-                {streakCount}일 달성
-              </span>
-              <span className="text-sm font-medium text-green-600 bg-blue-50 px-3 py-1 rounded-full">
-                {consecutiveDayCount}일 연속
-              </span>
-            </section>
-          </div>
-          <div className="h-100">
-            <VoronoiStreak streakCount={streakCount} imageSrc={imageSrc} />
-          </div>
-        </div>
-      </section>
-    </main>
+    <div className="w-4xl px-8 py-15 max-w-4xl flex flex-col gap-10">
+      <UserStatsCard
+        nickname={userData.nickname}
+        email={userData.email}
+        totalPoint={userData.totalPoint}
+        role={userData.role}
+        consecutiveDayCount={consecutiveDayCount}
+      />
+      <Tabs className="w-full" defaultValue="graph">
+        <TabsList>
+          <TabsTrigger className="flex gap-2.5" value="graph">
+            <CircleCheckBig className="w-4 h-4" />
+            지식 그래프
+          </TabsTrigger>
+          <TabsTrigger className="flex gap-2.5" value="streak">
+            <Brush className="w-4 h-4" />
+            명화 스트릭
+          </TabsTrigger>
+          <TabsTrigger className="flex gap-2.5" value="solvedList">
+            <BookText className="w-4 h-4" />
+            내가 푼 문제
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent
+          className="bg-white w-full px-8 pb-8 rounded-xl border border-slate-200"
+          value="graph"
+        >
+          <GraphContent graphData={mockData} />
+        </TabsContent>
+        <TabsContent
+          className="bg-white w-full px-8 pb-8 rounded-xl border border-slate-200"
+          value="streak"
+        >
+          <StreakContent streakCount={streakCount} imageSrc={imageSrc} />
+        </TabsContent>
+        <TabsContent
+          className="bg-white w-full px-8 pb-8 rounded-xl border border-slate-200"
+          value="solvedList"
+        >
+          <SolvedContent />
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 }
 
