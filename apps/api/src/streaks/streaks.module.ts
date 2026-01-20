aa82e969d63b1dfc44854c58f9f2d207a6f2b4e1
AYEOOON <k93853471@gmail.com>
2026-01-20 11:35:58 +0900

    feat(#184): 스트릭 관련 파일을 Bearer 토큰 인증 방식으로 전환
    - 모든 엔드포인트에 @ApiBearerAuth('JWT-auth') 적용
    - AuthModule import 추가
    - Bearer 토큰 인증 사용

--- apps/api/src/streaks/streaks.module.ts
+++ apps/api/src/streaks/streaks.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Streaks } from './entities/streaks.entity';
 import { StreaksController } from './streaks.controller';
 import { StreaksService } from './streaks.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Streaks])],
+  imports: [TypeOrmModule.forFeature([Streaks]), AuthModule],
   controllers: [StreaksController],
   providers: [StreaksService],
   exports: [StreaksService],
