caf62a542b5d1ae564ec8c621731605fd55f9e94
dltnwjd308 <dltnwjd308@naver.com>
2026-01-21 01:23:51 +0900

    feat(#208): login 페이지 구현 및 로고 삽입

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,31 +1,44 @@
 import { redirect } from "next/navigation";
-import { getCurrentUser, getTestUsers, login } from "../_utils/auth";
+import { getCurrentUser, login } from "../_utils/auth";
 import LoginForm from "../_components/login-form";
+import Image from "next/image";
+import Link from "next/link";
 
 async function LoginPage() {
   const user = await getCurrentUser();
+  const mmhLogo = "/mmh-logo.svg";
 
-  // 이미 로그인되어 있으면 홈으로 리다이렉트
   if (user) {
     redirect("/");
   }
 
-  const testUsers = await getTestUsers();
-
   return (
     <div className="flex min-h-screen flex-col bg-zinc-50 dark:bg-black">
-      <div className="flex flex-1 items-center justify-center">
-        <div className="w-full max-w-md rounded-lg border border-zinc-200 bg-white p-8 shadow-sm dark:border-zinc-800 dark:bg-zinc-900">
-          <div className="mb-6">
+      <div className="flex flex-1 items-center justify-center ">
+        <div className="flex flex-col justify-center items-center w-full max-w-md rounded-lg border border-zinc-200 bg-white p-8 shadow-sm dark:border-zinc-800 dark:bg-zinc-900">
+          <div className="flex gap-2 justify-center items-center mb-12">
+            <Image src={mmhLogo} alt="말만해 로고" width={32} height={32} />
+            <p className="text-xl text-slate-700 font-semibold">말만해</p>
+          </div>
+          <div className="flex flex-col gap-2 justify-center items-center mb-6 ">
             <h1 className="text-2xl font-semibold text-black dark:text-zinc-50">
-              테스트 로그인
+              로그인
             </h1>
             <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-              개발/테스트 전용 로그인 페이지입니다.
+              서비스 이용을 위해 로그인이 필요합니다.
             </p>
           </div>
 
-          <LoginForm testUsers={testUsers} loginAction={login} />
+          <LoginForm loginAction={login} />
+          <div className="mt-8 text-center text-sm text-zinc-500">
+            말만해 서비스가 처음이신가요?{" "}
+            <Link
+              href="/signup"
+              className="text-[#2DD4BF] font-semibold hover:underline"
+            >
+              회원가입하기
+            </Link>
+          </div>
         </div>
       </div>
     </div>
