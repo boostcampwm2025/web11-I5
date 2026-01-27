cd95aedfcac4c87c256eecd8ff839fbd28a1cb12
AYEOOON <k93853471@gmail.com>
2026-01-27 20:01:46 +0900

    fix: 누락된 새 의존성 추가
    - 테스트 실패 원인 : QuestionService 생성자가 의존성을 더 받도록 바뀌었는데, question.service.spec.ts에서 그 새 의존성들을 안 넣어줬기 때문
    - 새 의존성 mock 추가 및 TestModule에 주입

--- apps/api/src/question/question.service.spec.ts
+++ apps/api/src/question/question.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { AnswerSubmission } from 'src/answer-submission/entities/answer-submission.entity';
+import { AnswerSubmissionService } from 'src/answer-submission/answer-submission.service';
+import { User } from 'src/user/entities/user.entity';
 import { Question } from './entities/question.entity';
 import { SolvedStatus } from './question.constants';
 import { QuestionService } from './question.service';
@@ -37,6 +39,14 @@ describe('QuestionService', () => {
     createQueryBuilder: jest.fn(() => mockSubmissionQueryBuilder),
   };
 
+  const mockUserRepository = {
+    findOne: jest.fn(),
+  };
+
+  const mockAnswerSubmissionService = {
+    getSubmissionForOtherUser: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -48,6 +58,14 @@ describe('QuestionService', () => {
         {
           provide: getRepositoryToken(AnswerSubmission),
           useValue: mockAnswerSubmissionRepository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: AnswerSubmissionService,
+          useValue: mockAnswerSubmissionService,
         },
       ],
     }).compile();
