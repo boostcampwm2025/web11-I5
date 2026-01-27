612158ebf3cf37d43c6ce99b1cac07ab351ad5ad
AYEOOON <k93853471@gmail.com>
2026-01-27 18:44:09 +0900

    feat(#287): 타 사용자 제출 조회를 위해 Question 모듈에 User 엔티티 추가

--- apps/api/src/question/question.module.ts
+++ apps/api/src/question/question.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnswerSubmissionModule } from 'src/answer-submission/answer-submission.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { CategoryModule } from '../category/category.module';
+import { User } from '../user/entities/user.entity';
 import { Question } from './entities/question.entity';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Question]),
+    TypeOrmModule.forFeature([Question, User]),
     CategoryModule,
     AuthModule,
     AnswerSubmissionModule,
