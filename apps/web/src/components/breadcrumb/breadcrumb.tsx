43f630c3e04869825b25f1d8bca7630318889d6a
rwaeng <rwaeng0421@gmail.com>
2026-02-03 15:02:57 +0900

    feat(#350): Breadcrumb 컴포넌트 생성

---
+++ apps/web/src/components/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import { ChevronRight } from "lucide-react";
+import { cn } from "@/lib/cn";
+import Link from "next/link";
+
+function Breadcrumb({ className, ...props }: React.ComponentProps<"nav">) {
+  return (
+    <nav
+      aria-label="breadcrumb"
+      data-slot="breadcrumb"
+      className={cn("", className)}
+      {...props}
+    />
+  );
+}
+
+function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
+  return (
+    <ol
+      data-slot="breadcrumb-list"
+      className={cn(
+        "flex flex-wrap items-center gap-1 md:gap-1.5 text-xs md:text-sm text-muted-foreground",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function BreadcrumbItem({ className, ...props }: React.ComponentProps<"li">) {
+  return (
+    <li
+      data-slot="breadcrumb-item"
+      className={cn("inline-flex items-center gap-1.5", className)}
+      {...props}
+    />
+  );
+}
+
+function BreadcrumbLink({
+  className,
+  ...props
+}: React.ComponentProps<typeof Link>) {
+  return (
+    <Link
+      data-slot="breadcrumb-link"
+      className={cn("hover:underline", className)}
+      {...props}
+    />
+  );
+}
+
+function BreadcrumbPage({ className, ...props }: React.ComponentProps<"span">) {
+  return (
+    <span
+      role="link"
+      aria-current="page"
+      data-slot="breadcrumb-page"
+      className={cn("font-medium text-teal-600", className)}
+      {...props}
+    />
+  );
+}
+
+function BreadcrumbSeparator({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<"li">) {
+  return (
+    <li
+      role="presentation"
+      aria-hidden="true"
+      data-slot="breadcrumb-separator"
+      className={cn("", className)}
+      {...props}
+    >
+      {children ?? <ChevronRight className="h-3.5 w-3.5 md:h-4 md:w-4" />}
+    </li>
+  );
+}
+
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+};
